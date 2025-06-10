1c8fe741ca46a22e902a3b96406468c2d7a9585b
Enes Kıyak <enes.kyk026@gmail.com>
2025-06-10 13:31:58 +0300

    feat: added GPA calculation example in 'ortalama' package

---
+++ src/exercises/ortalama/calculator.java
@@ -0,0 +1,37 @@
+package exercises.ortalama;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+
+        int fizik, tarih, mat, muzik, kimya, turkce;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu girin");
+        mat = input.nextInt();
+
+        System.out.println("Fizik notunuzu girin");
+        fizik = input.nextInt();
+
+        System.out.println("Türkçe notunuzu girin");
+        turkce = input.nextInt();
+
+        System.out.println("Müzik notunuzu girin");
+        muzik = input.nextInt();
+
+        System.out.println("Kimya notunuzu girin");
+        kimya = input.nextInt();
+
+        System.out.println("Tarih notunuzu girin");
+        tarih = input.nextInt();
+
+        int sonuc = (tarih + kimya + mat + muzik + fizik + turkce) / 6;
+
+        String cikti = (sonuc > 60) ? ("Sınıfı Geçtiniz") : ("Sınıfta Kaldınız");
+
+        System.out.println(cikti);
+
+
+    }
+}
